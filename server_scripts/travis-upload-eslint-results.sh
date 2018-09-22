5ae4dc5fe632e528d06b9df91dad69ab6b01ffa6
Alex Frappier Lachapelle <narutoxela@gmail.com>
2018-09-22 00:03:43 -0400

    Created a util script for slack and updated the eslint result upload.

---
+++ server_scripts/travis-upload-eslint-results.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script executed by travis CI that uploads the results of eslint to the #build-status channel
+
+#Usage
+# travis-upload-eslint-results <slack API token> <slack channel tag>
+
+if [ "$#" -ne 3 ]; then
+    echo "USAGE: travis-upload-eslint-results <slack API token> <slack channel tag> <eslint result file path>"
+fi
+
+export TRAVIS_LINK="https://travis-ci.com/Cyberunner23/SOEN343/jobs/${TRAVIS_JOB_ID}"
+export SLACK_COMMENT="Eslint output for build #${TRAVIS_BUILD_NUMBER} at ${TRAVIS_LINK}"
+export BASEDIR=$(dirname "$0")
+
+${BASEDIR}/slack.sh $1 $2 "${SLACK_COMMENT}" $3
+
