9c1d15d51d5105b816213fad53ecc6828da0b922
David Payette <david.payette@outlook.com>
2018-10-09 13:30:57 -0700

    Refactor: UserController is now a singleton class. Using setInterval method instead of using node-cron for auto logout feature because setInterval doesn't require an extra dependency and suppports minimum intervals of 1 millisecond! Node-cron requires an extra dependency and only supports minimum intervals of 1 minute.

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,144 +1,157 @@
 const UserMapper = require('../mappers/UserMapper');
 const Exceptions = require('../Exceptions').Exceptions;
-const cron = require('node-cron');
-
-// every minute, automatically logout users that haven't been active for at least x minutes
-const x = 0; // made it 0 minutes for testing. We can change this LATER
-cron.schedule("* * * * *", () => {
-    userMapper.removeActiveUsers(user => {
-        var currentMilliseconds = new Date().getTime(); // creating a new date initializes time to current time
-        return (currentMilliseconds - user.timestamp.getTime()) >= x * 60 * 1000; // getTime() returns milliseconds
-    })
-    .then(activeUsers => {
-        activeUsers.forEach(user => {
-            console.log('User with id=' + user.id + ' automatically logged out');
-        })
-    })
-    .catch(exception => {
-        console.log('An exception occured when attempting to run auto-logout feature');
-    })
-});
 
 const userMapper = UserMapper.getInstance();
 
-exports.authenticate = async (req, res) => {
-    var users = userMapper.getUsers(user => {
-        return(
-            user.email === req.body.email &&
-            user.password === req.body.password
-        );
-    });
-    if (users.length === 0) {
-        console.log('invalid login');
-        res.status(400);
-        res.json({err: 'Invalid credentials'});
+// Every second, logout users that haven't been active for at least [maxInactivitySeconds]
+startAutoLogout = () => {
+    const maxInactivitySeconds = 30; // The max inactivity period is small to speed up testing. We can change this later.
+    setInterval(() => {
+        userMapper.removeActiveUsers(user => {
+            currentMilliseconds = new Date().getTime(); // creating a new date initializes time to current time
+            return (currentMilliseconds - user.timestamp.getTime()) >= maxInactivitySeconds * 1000; // getTime() returns milliseconds
+        })
+        .then(activeUsers => {
+            activeUsers.forEach(user => {
+                console.log('User with id=' + user.id + ' automatically logged out');
+            })
+        })
+        .catch(exception => {
+            console.log('An exception occured when attempting to run auto-logout feature');
+        })
+    }, 1000) // every 1000 milliseconds
+}
+
+class UserController {
+
+    constructor() {
+        startAutoLogout();
     }
-    else if (users.length === 1) {
-        var user = users[0];
-        refreshActivityStatus(user.id) // we will use authToken in the future
-        .then(() => {
-            console.log('login success');
-            res.status(200);
-            res.json(convertToFrontendUser(user));
+
+    async authenticate (req, res) {
+        var users = userMapper.getUsers(user => {
+            return(
+                user.email === req.body.email &&
+                user.password === req.body.password
+            );
+        });
+        if (users.length === 0) {
+            console.log('invalid login');
+            res.status(400);
+            res.json({err: 'Invalid credentials'});
+        }
+        else if (users.length === 1) {
+            var user = users[0];
+            refreshActivityStatus(user.id) // we will use authToken in the future
+            .then(() => {
+                console.log('login success');
+                res.status(200);
+                res.json(convertToFrontendUser(user));
+            })
+            .catch(exception => {
+                handleException(res, exception);
+            })
+        }
+        else {
+            console.log('There is more than one user with the same email and password in the database. Fix it!');
+            handleException(res, Exceptions.InternalServerError);
+        }
+    }
+    
+    async registerUser (req, res) {
+        refreshActivityStatus(req.body.authToken)
+        .then(activeUser => {
+            var result = userMapper.getUsers(user => {
+                return (
+                    user.email === req.body.email
+                )
+            });
+            if (result.length === 0) {
+                req.body.salt = 'bon matin'; // change this when encryption is implemented
+                userMapper.addUser(req.body)
+                .then((user) => {
+                    res.status(200);
+                    res.json(convertToFrontendUser(user));
+                })
+                .catch((exception) => {
+                    handleException(res, exception);
+                });
+            } else {
+                console.log('email already exists');
+                handleException(res, Exceptions.EmailAlreadyUsed);
+            }
         })
         .catch(exception => {
             handleException(res, exception);
         })
     }
-    else {
-        console.log('There is more than one user with the same email and password in the database. Fix it!');
-        handleException(res, Exceptions.InternalServerError);
+    
+    async activeUsers (req, res) {
+        refreshActivityStatus(req.body.authToken)
+        .then(activeUser => {
+            var activeUsers = userMapper.getActiveUsers(); // activeUsers only have index, id and timestamp. Must retrieve user info before sending to frontend
+            var userPairs = [];
+            
+            activeUsers.forEach(activeUser => {
+                var userArray = userMapper.getUsers(user => {
+                    return (
+                        user.id === activeUser.id
+                    )
+                });
+                if (userArray.length === 1) {
+                    var userPair = {user: userArray[0], activeUser};
+                    userPairs.push(userPair);
+                }
+                else {
+                    console.log('There should be exactly 1 user with the supplied id. Something is wrong.');
+                    throw Exceptions.InternalServerError;
+                }
+        
+            })
+            
+            res.status(200);
+            res.json(convertToFrontendActiveUsers(userPairs));
+        })
+        .catch(exception => {
+            handleException(res, exception);
+        })
     }
-}
-
-exports.registerUser = async (req, res) => {
-    refreshActivityStatus(req.body.authToken)
-    .then(activeUser => {
-        var result = userMapper.getUsers(user => {
+    
+    async logout (req, res) {
+    
+        var userArray = userMapper.getUsers(user => {
             return (
                 user.email === req.body.email
             )
         });
-        if (result.length === 0) {
-            req.body.salt = 'bon matin'; // change this when encryption is implemented
-            userMapper.addUser(req.body)
-            .then((user) => {
-                res.status(200);
-                res.json(convertToFrontendUser(user));
-            })
-            .catch((exception) => {
-                handleException(res, exception);
-            });
-        } else {
-            console.log('email already exists');
-            handleException(res, Exceptions.EmailAlreadyUsed);
+        if (userArray.length > 1) {
+            console.log('There is more than 1 user with the same email. Fix it!');
+            handleException(res, Exceptions.InternalServerError);
         }
-    })
-    .catch(exception => {
-        handleException(res, exception);
-    })
+        else if (userArray.length === 0) {
+            console.log('The requested user does not exist');
+            handleException(res, Exceptions.UserDoesNotExist);
+        }
+        else { // userArray.length must be 1
+            var userToLogout = userArray[0];
+            userMapper.removeActiveUsers(user => {
+                return user.id === userToLogout.id;
+            })
+            .then(() => {
+                console.log('logout success');
+                res.status(200);
+                res.send();
+            })
+            .catch(exception => {
+                handleException(res, exception);
+            })
+        }
+    }
 }
 
-exports.activeUsers = async (req, res) => {
-    refreshActivityStatus(req.body.authToken)
-    .then(activeUser => {
-        var activeUsers = userMapper.getActiveUsers(); // activeUsers only have index, id and timestamp. Must retrieve user info before sending to frontend
-        var userPairs = [];
-        
-        activeUsers.forEach(activeUser => {
-            var userArray = userMapper.getUsers(user => {
-                return (
-                    user.id === activeUser.id
-                )
-            });
-            if (userArray.length === 1) {
-                var userPair = {user: userArray[0], activeUser};
-                userPairs.push(userPair);
-            }
-            else {
-                console.log('There should be exactly 1 user with the supplied id. Something is wrong.');
-                throw Exceptions.InternalServerError;
-            }
-    
-        })
-        
-        res.status(200);
-        res.json(convertToFrontendActiveUsers(userPairs));
-    })
-    .catch(exception => {
-        handleException(res, exception);
-    })
-}
-
-exports.logout = async (req, res) => {
-
-    var userArray = userMapper.getUsers(user => {
-        return (
-            user.email === req.body.email
-        )
-    });
-    if (userArray.length > 1) {
-        console.log('There is more than 1 user with the same email. Fix it!');
-        handleException(res, Exceptions.InternalServerError);
-    }
-    else if (userArray.length === 0) {
-        console.log('The requested user does not exist');
-        handleException(res, Exceptions.UserDoesNotExist);
-    }
-    else { // userArray.length must be 1
-        var userToLogout = userArray[0];
-        userMapper.removeActiveUsers(user => {
-            return user.id === userToLogout.id;
-        })
-        .then(() => {
-            console.log('logout success');
-            res.status(200);
-            res.send();
-        })
-        .catch(exception => {
-            handleException(res, exception);
-        })
-    }
+const instance = new UserController()
+exports.getInstance = () => {
+    return instance;
 }
 
 handleException = function (res, exception) {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const UserController = require('./backend/controllers/UserController');
+const UserController = require('./backend/controllers/UserController').getInstance();
 
 const port = 5000;
 
