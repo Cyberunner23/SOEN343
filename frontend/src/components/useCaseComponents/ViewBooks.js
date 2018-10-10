5865ea3f29fecf8dbef316eaa08f799255447adb
Philippe Lorange <philippe.lorange@gmail.com>
2018-10-10 16:30:49 -0400

    added all book elements to front end

--- frontend/src/components/useCaseComponents/ViewBooks.js
+++ frontend/src/components/useCaseComponents/ViewBooks.js
@@ -6,7 +6,13 @@ export default class ActiveUsers extends Component {
         super(props);
         this.state = {
             title: '',
+            author: '',
+            format: '',
+            pages: '',
+            publisher: '',
+            language: '',
             isbn10: '',
+            isbn13: '',
             app: props.app,
             books: [],
             bookAdded: false
@@ -48,8 +54,32 @@ export default class ActiveUsers extends Component {
         <input type="text" name="title" onChange={this.handleChange} />
                         </label>
                         <label>
+                            Author:
+        <input type="text" name="author" onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Format:
+        <input type="text" name="format" onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Pages:
+        <input type="text" name="pages" onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Publisher:
+        <input type="text" name="publisher" onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Language:
+        <input type="text" name="language" onChange={this.handleChange} />
+                        </label>
+                        <label>
                             ISBN-10:
         <input type="text" name="isbn10" onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            ISBN-13:
+        <input type="text" name="isbn13" onChange={this.handleChange} />
                         </label>
                         <button onClick={this.handleSubmit}>Submit</button>
                     </form>
@@ -89,13 +119,19 @@ export default class ActiveUsers extends Component {
 
     async addBook(props) {
         let title = props.title;
+        let author = props.author;
+        let format = props.format;
+        let pages = props.pages;
+        let publisher = props.publisher;
+        let language = props.language;
         let isbn10 = props.isbn10;
+        let isbn13 = props.isbn13
 
         return new Promise((resolve, reject) => {
             fetch('/api/addBook', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({title, isbn10})
+                body: JSON.stringify({title, author, format, pages, publisher, language, isbn10, isbn13})
             }).then((response) => {
                 if (response.status === 200) {
                     response.json().then((book) => {
