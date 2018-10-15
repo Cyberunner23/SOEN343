e69cedffe981ea9f4fa3b105317b647d17a7b133
David Payette <david.payette@outlook.com>
2018-10-15 10:39:19 -0700

    Move persistance logic from UserMapper to UserGateway

---
+++ backend/gateways/UserGateway.js
@@ -0,0 +1,136 @@
+const mysql = require('mysql');
+const db = require('../DatabaseConnection').getInstance();
+const User = require('../business_objects/user').User;
+const ActiveUser = require('../business_objects/activeUser').ActiveUser;
+
+class UserGateway {
+
+    async loadUsers() {
+        return new Promise((resolve, reject) => {
+            var query = "SELECT * FROM users";
+            db.query(query, (err, result) => {
+                if (!err) {
+                    resolve(getUserArray(result));
+                }
+                else {
+                    console.log(err);
+                    reject(Exceptions.InternalServerError);
+                }
+            })
+        })
+    }
+
+    async loadActiveUsers() {
+        return new Promise((resolve, reject) => {
+            let sql = 'SELECT * FROM activeUsers';
+            db.query(sql, (err, result) => {
+                if (!err) {
+                    resolve(getActiveUserArray(result));
+                }
+                else {
+                    console.log(err);
+                    reject(Exceptions.InternalServerError);
+                }
+            });
+        })
+    }
+
+    async persistAddUser(jsonUser) {
+        return new Promise((resolve, reject) => {
+            var query = 'INSERT INTO users (is_admin, email, password, salt, first_name, last_name, phone, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+            var inserts = [jsonUser.is_admin, jsonUser.email, jsonUser.password, jsonUser.salt,
+                           jsonUser.first_name, jsonUser.last_name, jsonUser.phone, jsonUser.address];
+            query = mysql.format(query, inserts);
+            
+            db.query(query, (err, response) => {
+                if (err) {
+                    console.log(err);
+                    reject(Exceptions.InternalServerError);
+                } else {
+                    jsonUser.id = response.insertId;
+                    var newUser = new User(jsonUser);
+                    resolve (newUser);
+                }
+            });
+        })
+    }
+    
+    async persistAddActiveUser(jsonActiveUser) {
+        return new Promise((resolve, reject) => {
+            var query = 'INSERT INTO activeUsers (id, timestamp) VALUES (?, ?)'
+            var inserts = [jsonActiveUser.id, jsonActiveUser.timestamp];
+            query = mysql.format(query, inserts);
+            db.query(query, (err, response) => {
+                if (err) {
+                    console.log(err);
+                    reject(Exceptions.InternalServerError);
+                }
+                else {
+                    jsonActiveUser.index = response.insertId;
+                    var newActiveUser = new ActiveUser(jsonActiveUser);
+                    resolve (newActiveUser);
+                }
+            })
+        })
+    }
+    
+    async persistUpdateActiveUser(jsonActiveUser) {
+        return new Promise((resolve, reject) => {
+            var query = "UPDATE activeUsers SET timestamp=? WHERE id=?";
+            var inserts = [jsonActiveUser.timestamp, jsonActiveUser.id];
+            query = mysql.format(query, inserts);
+            db.query(query, (err, response) => {
+                if (err) {
+                    console.log(err);
+                    reject(Exceptions.InternalServerError);
+                }
+                else {
+                    resolve();
+                }
+            })
+        })
+    }
+    
+    async persistDeleteActiveUser(idsToRemove) {
+        return new Promise((resolve, reject) => {
+            var query;
+            if (idsToRemove) {
+                query = 'DELETE FROM activeUsers WHERE id IN (' + idsToRemove.join() + ')';
+            }
+            else {
+                query = 'DELETE FROM activeUsers'
+            }
+        
+            db.query(query, (err, result) => {
+                if (!err) {
+                    resolve();
+                }
+                else {
+                    console.log(err);
+                    reject(Exceptions.InternalServerError);
+                }
+            })
+        })
+    }
+}
+
+const instance = new UserGateway();
+exports.getInstance = () => {
+    return instance;
+}
+
+getUserArray = (jsonUsers) => {
+    var users = [];
+    jsonUsers.forEach((jsonUser) => {
+        users.push(new User(jsonUser));
+    })
+    return users;
+}
+
+getActiveUserArray = (jsonUsers) => {
+    var users = [];
+    jsonUsers.forEach((jsonUser) => {
+        users.push(new ActiveUser(jsonUser));
+    })
+    return users;
+}
--- backend/mappers/UserMapper.js
+++ backend/mappers/UserMapper.js
@@ -1,21 +1,15 @@
-const mysql = require('mysql');
-const DatabaseConnection = require('../DatabaseConnection');
-const User = require('../business_objects/user').User;
-const ActiveUser = require('../business_objects/activeUser').ActiveUser;
-
+const userGateway = require('../gateways/UserGateway').getInstance();
 const Exceptions = require('../Exceptions').Exceptions;
-
-const db = DatabaseConnection.getInstance();
 
 class UserMapper {
 
     constructor() {
         // The InventoryMapper won't need to load any resources from the database for iteration 3
         // Just initiazile empty arrays instead (one for each item type)
-        loadUsers().then((users) => {
+        userGateway.loadUsers().then((users) => {
             this.users = users;
         });
-        loadActiveUsers().then((users) => {
+        userGateway.loadActiveUsers().then((users) => {
             this.activeUsers = users;
         });
     }
@@ -49,7 +43,7 @@ class UserMapper {
     // The InventoryMapper will need 4 add methods (one for each item type)
     async addUser (jsonUser) {
         return new Promise((resolve, reject) => {
-            persistAddUser(jsonUser)
+            userGateway.persistAddUser(jsonUser)
             .then(newUser => {
                 this.users.push(newUser);
                 resolve(newUser);
@@ -62,7 +56,7 @@ class UserMapper {
 
     async addActiveUser (jsonActiveUser) {
         return new Promise((resolve, reject) => {
-            persistAddActiveUser(jsonActiveUser)
+            userGateway.persistAddActiveUser(jsonActiveUser)
             .then(newActiveUser => {
                 this.activeUsers.push(newActiveUser);
                 resolve(newActiveUser);
@@ -87,7 +81,7 @@ class UserMapper {
                 reject(Exceptions.InternalServerError);
             }
             else { // activeUsersWithMatchingId.length must be 1
-                persistUpdateActiveUser(jsonActiveUser)
+                userGateway.persistUpdateActiveUser(jsonActiveUser)
                 .then(() => {
                     var index = this.activeUsers.findIndex(user => {
                         return user.id === jsonActiveUser.id;
@@ -136,7 +130,7 @@ class UserMapper {
                 })
             }
 
-            persistDeleteActiveUser(idsToRemove)
+            userGateway.persistDeleteActiveUser(idsToRemove)
             .then(() => {
                 resolve(removedUsers);
             })
@@ -147,144 +141,7 @@ class UserMapper {
     }
 }
 
-getUserArray = (jsonUsers) => {
-    var users = [];
-    jsonUsers.forEach((jsonUser) => {
-        users.push(new User(jsonUser));
-    })
-    return users;
-}
-
-loadUsers = async () => {
-    return new Promise((resolve, reject) => {
-        var query = "SELECT * FROM users";
-        db.query(query, (err, result) => {
-            if (!err) {
-                resolve(getUserArray(result));
-            }
-            else {
-                console.log(err);
-                reject(Exceptions.InternalServerError);
-            }
-        })
-    })
-}
-
-getActiveUserArray = (jsonUsers) => {
-    var users = [];
-    jsonUsers.forEach((jsonUser) => {
-        users.push(new ActiveUser(jsonUser));
-    })
-    return users;
-}
-
-loadActiveUsers = async () => {
-    return new Promise((resolve, reject) => {
-        let sql = 'SELECT * FROM activeUsers';
-        db.query(sql, (err, result) => {
-            if (!err) {
-                resolve(getActiveUserArray(result));
-            }
-            else {
-                console.log(err);
-                reject(Exceptions.InternalServerError);
-            }
-        });
-    })
-}
-
-persistAddUser = async (jsonUser) => {
-    return new Promise((resolve, reject) => {
-        var query = 'INSERT INTO users (is_admin, email, password, salt, first_name, last_name, phone, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        var inserts = [jsonUser.is_admin, jsonUser.email, jsonUser.password, jsonUser.salt,
-                       jsonUser.first_name, jsonUser.last_name, jsonUser.phone, jsonUser.address];
-        query = mysql.format(query, inserts);
-        
-        db.query(query, (err, response) => {
-            if (err) {
-                console.log(err);
-                reject(Exceptions.InternalServerError);
-            } else {
-                jsonUser.id = response.insertId;
-                var newUser = new User(jsonUser);
-                console.log('newUser: ' + JSON.stringify(newUser));
-                resolve (newUser);
-            }
-        });
-    })
-}
-
-persistAddActiveUser = async (jsonActiveUser) => {
-    return new Promise((resolve, reject) => {
-        var query = 'INSERT INTO activeUsers (id, timestamp) VALUES (?, ?)'
-        var inserts = [jsonActiveUser.id, jsonActiveUser.timestamp];
-        query = mysql.format(query, inserts);
-        db.query(query, (err, response) => {
-            if (err) {
-                console.log(err);
-                reject(Exceptions.InternalServerError);
-            }
-            else {
-                jsonActiveUser.index = response.insertId;
-                var newActiveUser = new ActiveUser(jsonActiveUser);
-                resolve (newActiveUser);
-            }
-        })
-    })
-}
-
-persistUpdateActiveUser = async (jsonActiveUser) => {
-    return new Promise((resolve, reject) => {
-        var query = "UPDATE activeUsers SET timestamp=? WHERE id=?";
-        var inserts = [jsonActiveUser.timestamp, jsonActiveUser.id];
-        query = mysql.format(query, inserts);
-        db.query(query, (err, response) => {
-            if (err) {
-                console.log(err);
-                reject(Exceptions.InternalServerError);
-            }
-            else {
-                resolve();
-            }
-        })
-    })
-}
-
-persistDeleteActiveUser = async (idsToRemove) => {
-    return new Promise((resolve, reject) => {
-        var query;
-        if (idsToRemove) {
-            query = 'DELETE FROM activeUsers WHERE id IN (' + idsToRemove.join() + ')';
-        }
-        else {
-            query = 'DELETE FROM activeUsers'
-        }
-    
-        db.query(query, (err, result) => {
-            if (!err) {
-                resolve();
-            }
-            else {
-                console.log(err);
-                reject(Exceptions.InternalServerError);
-            }
-        })
-    })
-}
-
 const instance = new UserMapper();
-
 exports.getInstance = () => {
     return instance;
 }
-
-// if (json) {
-//     query += " WHERE ";
-
-//     var keys = Object.keys(json);
-//     for(var i = 0; i < keys.length -1; i++) {
-//         query += keys[i] + "='" + json[keys[i]] + "' AND ";
-//     }
-//     var last = keys.length -1;
-//     query += keys[last] + "='" + json[keys[last]] + "'";
-// }
