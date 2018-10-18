ed75d32bd2f08a919f95485ebc1f398325b98ec3
gdzooks <issa4044@gmail.com>
2018-10-18 02:09:36 -0400

    Update server.js methods and catalogue controller's require UserController

--- backend/controllers/CatalogueController.js
+++ backend/controllers/CatalogueController.js
@@ -1,7 +1,7 @@
 //<editor-fold desc="Constants">
 const catalogueMapper = require('../mappers/CatalogueMapper').getInstance();
 
-const userController = require('UserController').getInstance();
+const userController = require('./UserController').getInstance();
 
 const Exceptions = require('../Exceptions').Exceptions;
 //</editor-fold>
