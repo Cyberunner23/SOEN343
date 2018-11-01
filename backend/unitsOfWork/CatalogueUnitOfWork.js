4cbcf1757c26e8b30baa74b461379a46655cf9eb
Marlin Jayasekera <marlin.j.281@gmail.com>
2018-11-01 10:30:02 -0400

    Catalogue Unit of Work created but not integrated

--- backend/unitsOfWork/CatalogueUnitOfWork.js
+++ backend/unitsOfWork/CatalogueUnitOfWork.js
@@ -1,5 +1,6 @@
-const catalogueMapper = require('../mappers/CatalogueMapper').getInstance()
-const Operation = Object.freeze({
+const catalogueMapper = require('../mappers/CatalogueMapper').getInstance();
+const Exceptions = require('../Exceptions').Exceptions;
+const OperationType= Object.freeze({
     'Add' : 0,
     'Update' : 1,
     'Delete' : 2
@@ -9,7 +10,7 @@ class CatalogueUnitOfWork {
     constructor() {
         // Operations are jsons with an operation type and an identifier
         // For example, an add book operation has the following format:
-        // {operationType: Operation.Add, identifier: isbn13 }
+        // {operationType: OperationType.Add, identifier: isbn13 }
         this.bookOperations = []; // book identifier: isbn13
         this.magazineOperations = []; // magazine identifier: isbn13
         this.musicOperations = []; // music identifier: asin
@@ -34,19 +35,108 @@ class CatalogueUnitOfWork {
         })
     }
 
-    // Only 1 operation can be associated with a unique catalogue item at any given time
-    // add, update and delete are all valid operations for an identifier that wasn't already used
+    addItem(operationsArray, identifierValue){
+        if(!operationsArray.find(i => {return i['identifier'] === identifierValue;})) 
+            operationsArray.push({'operationType': OperationType.Add, 'identifier': identifierValue});
+        else if(operationsArray.some(i => 
+            { return i['identifier'] === identifierValue && (i['operationType'] === OperationType.Add || i['operationType'] === OperationType.Update);})){
+            throw Exceptions.Unauthorized;
+        }
+        else if(operationsArray.some(i => { return i['identifier'] === identifierValue && i['operationType'] === OperationType.Delete;})){
+            var item = operationsArray.findIndex(i => {
+                return i['identifier'] === identifierValue && i['operationType'] === OperationType.Delete;
+            });
+            operationsArray[item] = {'operationType': OperationType.Add, 'identifier': identifierValue};
+        }
+        else throw Exceptions.InvalidRequestBody;
+    }
 
+    updateItem(operationsArray, identifierValue){ 
+        // update operations override add and previous update operations, but attempting to override a delete operation should throw an exception
+        if(!operationsArray.find(i => {return i['identifier'] === isbn13;})) 
+            operationsArray.push({'operationType': OperationType.Update, 'identifier': isbn13});
+        else if(operationsArray.some(i => 
+            { return i['identifier'] === isbn13 && (i['operationType'] === OperationType.Add || i['operationType'] === OperationType.Update);})){
+                var item = operationsArray.findIndex(i => {
+                    return i['identifier'] === isbn13 && (i['operationType'] === OperationType.Add || i['operationType'] === OperationType.Update);
+                });
+                operationsArray[item] = {'operationType': OperationType.Update, 'identifier': isbn13};
+        }
+        else if(operationsArray.some(i => { return i['identifier'] === isbn13 && i['operationType'] === OperationType.Delete;})){
+            throw Exceptions.Unauthorized;
+        }
+        else throw Exceptions.InvalidRequestBody;
+    }
+
+    deleteItem(operationsArray, identifierValue){
+        // delete operations override add and update operations, but attempting to override a previous delete should throw an exception
+        if(!operationsArray.find(i => {return i['identifier'] === identifierValue;})) 
+            operationsArray.push({'operationType': OperationType.Delete, 'identifier': identifierValue});
+        else if(operationsArray.some(i => 
+            { return i['identifier'] === identifierValue && (i['operationType'] === OperationType.Add || i['operationType'] === OperationType.Update);})){
+                var item = operationsArray.findIndex(i => {
+                    return i['identifier'] === identifierValue && (i['operationType'] === OperationType.Add || i['operationType'] === OperationType.Update);
+                });
+                operationsArray[item] = {'operationType': OperationType.Delete, 'identifier': identifierValue};
+        }
+        else if(operationsArray.some(i => { return i['identifier'] === identifierValue && i['operationType'] === OperationType.Delete;})){
+            throw Exceptions.Unauthorized;
+        }
+        else throw Exceptions.InvalidRequestBody;
+    }
+
+    // Only 1 operation can be associated with a unique catalogue item at any given time
+
+    // Book Operations
     addBook (isbn13) {
-        // add operations override delete operations, but attempting to override a previous add or update should throw an exception
+        this.addItem(this.bookOperations, isbn13);
     }
 
     updateBook (isbn13) {
-        // update operations override add and previous update operations, but attempting to override a delete operation should throw an exception
+        this.updateItem(this.bookOperations, isbn13);
     }
 
     deleteBook (isbn13) {
-        // delete operations override add and update operations, but attempting to override a previous delete should throw an exception
+        this.deleteItem(this.bookOperations, isbn13);
+    }
+
+    //Magazine
+    addMagazine (isbn13) {
+        this.addItem(this.magazineOperations, isbn13);
+    }
+
+    updateMagazine (isbn13) {
+        this.updateItem(this.magazineOperations, isbn13);
+    }
+
+    deleteMagazine (isbn13) {
+        this.deleteItem(this.magazineOperations, isbn13);
+    }
+
+     // Music Operations
+    addMusic (asin) {
+        this.addItem(this.musicOperations, asin);
+    }
+
+    updateMusic (asin) {
+        this.updateItem(this.musicOperations, asin);
+    }
+
+    deleteMusic (asin) {
+        this.deleteItem(this.musicOperations, asin);
+    }
+
+    // Movie Operations
+    addMovie (eidr) {
+        this.addItem(this.movieOperations, eidr);
+    }
+
+    updateMovie (eidr) {
+        this.updateItem(this.movieOperations, eidr);
+    }
+
+    deleteMovie (eidr) {
+        this.deleteItem(this.movieOperations, eidr);
     }
 
     // You also need add, update and delete methods for magazines, musics and movies
