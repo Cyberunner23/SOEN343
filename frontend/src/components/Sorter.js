2dd561e961458650445ec77577481a85193a0512
David Payette <david.payette@outlook.com>
2018-11-03 03:58:35 -0700

    Simplify reverse when order is descending.

--- frontend/src/components/Sorter.js
+++ frontend/src/components/Sorter.js
@@ -4,37 +4,23 @@ class Sorter {
     //string for field to sort by (ex: book.title) -> use switch case?
     //boolean for ascending/descending -> change sorting type
 
-    stringSort(jsonArray, field, order) {  
-        if(order == true){
-
-            jsonArray.sort((record1, record2) => {
+    stringSort(jsonArray, field, descending) {
+        jsonArray.sort((record1, record2) => {
             record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
-        });
-
-        }else{
-            jsonArray.sort((record1, record2) => {
-                record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
-            }); //sorts the array first
-                jsonArray.reverse((record1, record2) => {
-                record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
-            }); //reverses the sorted array
+        })
+        if (descending) {
+            jsonArray.reverse();
         }
-
         return console.log(jsonArray);
     }
 
-    infSort(jsonArray, field, order){
-        if(order == true){
-            jsonArray.sort((record1, record2) => {
-                return record1.field -record2.field;//ascending order
-            });
+    infSort(jsonArray, field, descending){
+        jsonArray.sort((record1, record2) => {
+            return record1.field -record2.field;//ascending order
+        });
+        if (descending) {
+            jsonArray.reverse();
         }
-        else{
-            jsonArray.sort((record1, record2) => {
-                return record2.field -recordq.field;//descending order
-            });
-        }
-
         return console.log(jsonArray);
     }
 }
