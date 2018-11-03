8bcabc10ba6aecdc5c827ffe3d369324fe06c881
David Payette <david.payette@outlook.com>
2018-11-03 03:51:36 -0700

    Fix indentation

--- frontend/src/components/Sorter.js
+++ frontend/src/components/Sorter.js
@@ -1,46 +1,44 @@
 
 class Sorter {
-        //json is for array of records 
-        //string for field to sort by (ex: book.title) -> use switch case?
-        //boolean for ascending/descending -> change sorting type
+    //json is for array of records 
+    //string for field to sort by (ex: book.title) -> use switch case?
+    //boolean for ascending/descending -> change sorting type
 
-  function stringSort(jsonArray, field, order) {  
-    if(order == true){
+    function stringSort(jsonArray, field, order) {  
+        if(order == true){
 
-      jsonArray.sort((record1, record2) => {
-      record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
-       });
+            jsonArray.sort((record1, record2) => {
+            record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
+        });
 
-    }else{
-      jsonArray.sort((record1, record2) => {
-        record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
-         }); //sorts the array first
-     jsonArray.reverse((record1, record2) => {
-     record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
-        }); //reverses the sorted array
-    }
-    
-   return console.log(jsonArray);
-  }
- 
-  function infSort(jsonArray, field, order){
-    if(order == true){
-      jsonArray.sort((record1, record2) => {
-        return record1.field -record2.field;//ascending order
-      });
-    }
-    else{
-      jsonArray.sort((record1, record2) => {
-        return record2.field -recordq.field;//descending order
-      });
+        }else{
+            jsonArray.sort((record1, record2) => {
+                record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
+            }); //sorts the array first
+                jsonArray.reverse((record1, record2) => {
+                record1.field.localeCompare(record2.field, 'fr', {ignorePunctuation: true})
+            }); //reverses the sorted array
+        }
+
+        return console.log(jsonArray);
     }
 
-    return console.log(jsonArray);
- }
+    function infSort(jsonArray, field, order){
+        if(order == true){
+            jsonArray.sort((record1, record2) => {
+                return record1.field -record2.field;//ascending order
+            });
+        }
+        else{
+            jsonArray.sort((record1, record2) => {
+                return record2.field -recordq.field;//descending order
+            });
+        }
 
+        return console.log(jsonArray);
+    }
 }
- 
 
- exports.Sorter=Sorter;   
 
-  
+exports.Sorter=Sorter;   
+
