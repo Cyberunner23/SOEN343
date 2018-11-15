d2f2983eb4d19355ef3460d61f92978e55162151
Alex Frappier Lachapelle <narutoxela@gmail.com>
2018-11-15 00:02:40 -0500

    Added some attributes to business objects

--- backend/business_objects/LoanItem.js
+++ backend/business_objects/LoanItem.js
@@ -1,6 +1,15 @@
-class LoanItem {
-    constructor(props) {
-        this.id = props.id;
+// One instance of this class will exist for each item in the cart, including multiples of the same book, music, etc.
+class LoanItem 
+{
+    constructor(props) 
+    {
+        // The user that loaned this item.
+        this.userID = props.userID;
+        // ID of this cart item.
+        this.loanItemID = props.loanItemID;
+        // ID of the item type in the cart
+        this.itemID = props.itemID;
+
         // continue...
     }
 }
