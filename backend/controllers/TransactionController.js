d2dce18b0c0cff7490b49109ab4ec2e08158b5ea
Arrow105 <sean.how0@gmail.com>
2018-11-20 17:11:57 -0500

    Added first revision of return method for TransactionController.js

--- backend/controllers/TransactionController.js
+++ backend/controllers/TransactionController.js
@@ -124,8 +124,80 @@ class TransactionController {
                 return;
             }
 
-            // req.body.recordId
+			//This grabs the numAvailable count from a media item, to increment it 
+			var available;
+			var filter = {recordType: mediaType, identifier: mediaId};
+			//Not entirely sure if my use of genericMapper is correct here, please advise
+			await this.genericMapper.get(filter)
+			.then(record => {
+				available = record.numAvailable;
+			})
+			.catch(ex => {
+				handleException(res, ex);
+			})
+			available = available+1;
+			//Not entirely sure if my use of genericMapper is correct here, please advise
+			this.genericMapper.modify(filter, numAvailable: available)
+			.then(updatedRecords => {
+				if (updatedRecords.length === 0) {
+					handleException(res, Exceptions.BadRequest);
+				}
+				else {
+					res.status(200);
+					res.json(updatedRecords[0]); // There should be only 1 record because the filter is a unique identifier
+				}
+			})
+			
+			//Modify oldest existing borrow record to be set as returned
+			//First find the oldest borrow record's id			
+			var idToChange; //This will be the transactionId of the oldest active borrow
+			var timeOfId; //Contains the timestamp of transactionId to be changed
+			var filter2 = {transactionType:0 isReturned: 0 userId: userId, mediaId: mediaId};
+			await this.mapper.get(filter2)
+			.then(values => {
+				idToChange = values[0].transactionId;
+				timeOfId = values[0].transactionTime;
+				//This for loop compares the parsed values of each timestamp;
+				//	the smallest, aka. oldest, one is desired
+				//Starting with the first grabbed id, each one is compared in a loop
+				for (int i = 0, i< (values.length-1), i++){
+					if (Date.parse(timeOfId) > Date.parse(values[i+1].transactionTime)) {
+						idToChange = values[i+1].transactionId;
+						timeOfId = values[i+1].transactionTime;
+					}
+				}
+			})			
+			.catch(ex => {
+				handleException(res, ex);
+			})
+			
+			//Now that the transactionId has been found, set isReturned to 1
+			filter2 = {transactionId: idToChange} //Set the filter to find the desired id
+			this.mapper.modify(filter2, isReturned: 1)
+			.then(updatedRecords => {
+				if (updatedRecords.length === 0) {
+					handleException(res, Exceptions.BadRequest);
+				}
+				else {
+					res.status(200);
+					res.json(updatedRecords[0]); // There should be only 1 record because the filter is a unique identifier
+				}
+			})		
+			.catch(ex => {
+				handleException(res, ex);
+			})
+
+			//Adding a new return transaction to the db
+			this.mapper.add(new this.Transaction(req.body))
+			.then(record => {
+				res.status(200);
+				res.json(record);
+			})
+			.catch(ex => {
+				handleException(res, ex);
+			})			
         })
+		
         .catch(ex => 
         {
             handleException(res, ex);
